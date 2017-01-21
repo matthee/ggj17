17dd484f52bf1ac73c763aebd3fd9308242c639e
Christoph Thaller <christoph.thaller@student.tugraz.at>
2017-01-21 23:51:01 +0100

    Added missing explosion sources

---
+++ Assets/Explosion System/Scripts/PhysicalExplosion.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class PhysicalExplosion : MonoBehaviour 
+{
+    public float Radius;// explosion radius
+    public float Force;// explosion forse
+    void Update () 
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, Radius);// create explosion
+        for(int i=0; i<hitColliders.Length; i++)
+        {              
+            if(hitColliders[i].CompareTag("Player"))// if tag CanBeRigidbody
+            {
+				Player control = hitColliders [i].GetComponent<Player> ();
+				control.Die ();
+                if(!hitColliders[i].GetComponent<Rigidbody>())
+                {
+                hitColliders[i].gameObject.AddComponent<Rigidbody>();
+                }
+				hitColliders[i].GetComponent<Rigidbody>().AddForce(Vector3.up * Force); // push game object
+            }
+			
+        }
+        Destroy(gameObject,0.2f);// destroy explosion
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position,Radius);
+    }
+}
