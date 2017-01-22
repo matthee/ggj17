fcea89776a1b22a412dc84199b6c931ec11f0f71
Matthias Link <matthias@link.co.at>
2017-01-22 14:22:10 +0100

    Remove debug output

--- Assets/Scripts/TriggerSpawner.cs
+++ Assets/Scripts/TriggerSpawner.cs
@@ -6,7 +6,6 @@ public class TriggerSpawner : MonoBehaviour {
   public BarrelContainerSpawner spawner;
 
   public void OnTriggerEnter(Collider other) {
-    Debug.Log("Collision detected");
     if (other.gameObject.tag == "Player") {
       spawner.Release ();
     }
